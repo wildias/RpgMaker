40da4e70764dfce607bbdf62e4598e50f2b21f31
Wil_Dias <wildiasrca@gmail.com>
2026-01-15 09:42:42 -0300

    Criando copia

--- RpgMaker.Api/Program.cs
+++ RpgMaker.Api/Program.cs
@@ -65,7 +65,6 @@ builder.Services.AddCors(options =>
 //    );
 //});
 
-
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
